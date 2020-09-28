0afa1114122806d8ceb851a7be246d8b54358703
Agustin Alexander <agustin.alexander@mulesoft.com>
2020-09-28 14:29:58 -0300

    fix typo

--- module.c
+++ module.c
@@ -1,14 +1,14 @@
-# include < linux / module .h >
-# include < linux / kernel .h >
-int init_module ( void )
+#include <linux/module.h>
+#include <linux/kernel.h>
+int init_module(void)
 { /* Constructor */
-  printk ( KERN_INFO " UNGS : Driver registrado \n") ;
+  printk(KERN_INFO "UNGS : Driver registrado\n");
   return 0;
 }
-void cleanup_module ( void )
-{/* Destructor */
-  printk ( KERN_INFO " UNGS : Driver desregistrado \n");
+void cleanup_module(void)
+{ /* Destructor */
+  printk(KERN_INFO "UNGS : Driver desregistrado\n");
 }
-MODULE_LICENSE (" GPL ");
-MODULE_AUTHOR (" UNGS ");
-MODULE_DESCRIPTION ("Un primer driver ");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("UNGS");
+MODULE_DESCRIPTION("Un primer driver");
