9c9d1fe8eed41a82983ac2621856154e0bf11958
jrmncos <costillagermanalfredo@gmail.com>
2020-10-05 22:09:26 -0300

    Script funcionando, hay un bugg de buffer overflow

--- scriptMountCharDev.sh
+++ scriptMountCharDev.sh
@@ -2,15 +2,19 @@
 
 echo 'Ejecutando programa de montaje de dispositivo de caracter.'
 echo ''
-make clean --silent & make --silent
-insmod chardev.ko
+#sudo -E make clean --silent & -E make --silent
+sudo -E make clean
+sudo -E make
+sudo insmod miModulo.ko
 
 echo '#!/bin/bash' > tmp.sh
-chmod 777 tmp.sh
-dmesg | cut -c16-99 | grep sudo >> tmp.sh
+sudo chmod 777 tmp.sh
+sudo dmesg | cut -c16-99 | grep sudo >> tmp.sh
 ./tmp.sh
-
-echo 'Hola! Soy CharDev y me ejecute con exito.' > /dev/UNGS
-echo 'Proba con cat /dev/UNGS para verificarlo.'
-echo 'Recorda desmontar el dispositivo con rmmod chardev.'
+sudo -E make clean
+sudo rm tmp.sh
+clear
+echo 'El CharDevice-CaesarCipher se cargó con exito, para encriptar ejecuta echo "mensaje" > /dev/UNGS '
+echo 'Luego, proba con cat /dev/UNGS para verificarlo.'
+echo 'Recorda desmontar el dispositivo con sudo rmmod miModulo.'
 
